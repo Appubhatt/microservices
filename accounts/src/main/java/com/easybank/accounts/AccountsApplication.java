f96f0ab742de2b0e6c107b3dd0b17851e98fa2c5
Apurva Bhatt <apurva.bhatt@silvertouch.com>
2024-12-09 19:15:26 +0530

    documentation

--- accounts/src/main/java/com/easybank/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/easybank/accounts/AccountsApplication.java
@@ -1,9 +1,18 @@
 package com.easybank.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts Microservice",
+				description = "This is the account microservice of bank application which contains relationship between the customer and account",
+				version = "v1"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/com/easybank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/easybank/accounts/controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.easybank.accounts.constants.AccountsConstants;
 import com.easybank.accounts.dto.CustomerDto;
 import com.easybank.accounts.dto.ResponseDto;
 import com.easybank.accounts.service.IAccountService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -19,9 +23,15 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @Validated
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+
+@Tag(
+        name = "Accounts Service CRUD Operation.",
+        description = "This controller contains the create, read, update and delete operation of Accounts Service"
+)
 public class AccountsController {
 
     private IAccountService accountService;
+
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody  CustomerDto customerDto){

c3443a5c29a701c27e256cb5dafddb6c1305ac38
Appubhatt <apuva.bhatt3002@gmail,com>
2024-12-09 22:25:17 +0530

    card service added

--- card/src/main/java/com/easybank/card/CardApplication.java
+++ card/src/main/java/com/easybank/card/CardApplication.java
@@ -2,12 +2,13 @@ package com.easybank.card;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class CardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CardApplication.class, args);
-    }
 
+    }
 }

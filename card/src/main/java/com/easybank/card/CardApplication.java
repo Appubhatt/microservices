d385828cf8e11e60b8f2f2cf1cedd9d05a95a939
Appubhatt <apuva.bhatt3002@gmail,com>
2024-12-10 08:44:32 +0530

    card entity and schema created

--- card/src/main/java/com/easybank/card/CardApplication.java
+++ card/src/main/java/com/easybank/card/CardApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class CardApplication {
 
     public static void main(String[] args) {
---
+++ card/src/main/java/com/easybank/card/entity/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.easybank.card.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Column
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column
+    @CreatedBy
+    private String createdBy;
+
+    @Column
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column
+    @LastModifiedBy
+    private String updatedBy;
+}
